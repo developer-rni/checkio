2907ffc14394d181ccbafca82ab855fd14a7f7e3
developer-rni <developer.rni@gmail.com>
2021-06-08 13:04:40 +0300

    solved 3 exercise

--- py_checkio_solutions/Home/days_diff.py
+++ py_checkio_solutions/Home/days_diff.py
@@ -1,8 +1,12 @@
 #!/home/nikita/github/checkio/venv/bin/checkio --domain=py run days-diff
 
-# How old are you in a number of days? It's easy to calculate - just subtract your birthday from today. We could make    this a real challenge though and count the difference between any dates.
+# How old are you in a number of days? It's easy to calculate - just subtract your birthday from today. We could make
+# this a real challenge though and count the difference between any dates.
 # 
-# You are given two dates as an array with three numbers - a year, month and day. For example: 19 April 1982 will be    (1982, 4, 19). You should find the difference in days between the given dates. For example between today and    tomorrow = 1 day. The difference will always be either a positive number or zero, so don't forget about the absolute    value.
+# You are given two dates as an array with three numbers - a year, month and day. For example: 19 April 1982 will be
+# (1982, 4, 19). You should find the difference in days between the given dates. For example between today and
+# tomorrow = 1 day. The difference will always be either a positive number or zero, so don't forget about the
+# absolute    value.
 # 
 # Input:Two dates as tuples of integers.
 # 
@@ -12,10 +16,11 @@
 # 
 # 
 # END_DESC
+import datetime
+
 
 def days_diff(a, b):
-    # your code here
-    return None
+    return abs((datetime.date(*b) - datetime.date(*a)).days)
 
 
 if __name__ == '__main__':
--- py_checkio_solutions/Home/first_word.py
+++ py_checkio_solutions/Home/first_word.py
@@ -4,7 +4,9 @@
 # 
 # When solving a task pay attention to the following points:
 # 
-# There can be dots and commas in a string.A string can start with a letter or, for example, a dot or space.A word can contain an apostrophe and it's a part of a word.The whole text can be represented with one word and that's it.Input:A string.
+# There can be dots and commas in a string.A string can start with a letter or, for example, a dot or space.A word
+# can contain an apostrophe and it's a part of a word.The whole text can be represented with one word and that's
+# it.Input:A string.
 # 
 # Output:A string.
 # 
@@ -17,14 +19,30 @@ def first_word(text: str) -> str:
     """
         returns the first word in a given text.
     """
-    # your code here
-    return text[0:2]
+    index_word = 0
+    chars = set("abcdefghijklmnopqrstuvwxyz\\'")
+    for word in text.strip().split():
+        if any((c in chars) for c in word.lower()):
+            break
+        else:
+            index_word += 1
+    text = text.strip().split()[index_word]
+    if text.find('.'):
+        text = text.split('.')[0]
+    for i in range(0, -2, -1):
+        for j in text[i]:
+            if j == '.' or j == ',':
+                if i == 0:
+                    text = text[1:]
+                else:
+                    text = text[:-1]
+    return text
 
 
 if __name__ == '__main__':
     print("Example:")
     print(first_word("Hello world"))
-    
+
     # These "asserts" are used for self-checking and not for an auto-testing
     assert first_word("Hello world") == "Hello"
     assert first_word(" a word ") == "a"
